97fabf49673f1f90d69a456c7adf034ec235c673
An Dae Hun <dkseogns88@naver.com>
2023-05-06 19:34:07 +0900

    드래그방향에 따른 화살표 표시기능

--- AndroidTermProject/app/src/main/java/com/example/swiping_break_bricks/GameView.java
+++ AndroidTermProject/app/src/main/java/com/example/swiping_break_bricks/GameView.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Picture;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.RectF;
@@ -24,6 +25,13 @@ public class GameView extends View {
     private Bitmap brickBitmap;
     private PointF ballPosition;
     private PointF ballVelocity;
+
+
+    private boolean drawArrow = false;
+    private PointF initialTouch = new PointF();
+    private PointF currentTouch = new PointF();
+    private Paint arrowPaint;
+
     private ArrayList<RectF> bricks;
     private boolean ballIsMoving = false;
     private float initialTouchX, initialTouchY;
@@ -34,6 +42,7 @@ public class GameView extends View {
     public GameView(Context context) {
         super(context);
 
+        //화면
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         Point size = new Point();
@@ -41,42 +50,51 @@ public class GameView extends View {
         screenWidth = size.x;
         screenHeight = size.y;
 
+        //공
         int ballSize = Math.min(screenWidth, screenHeight) / 15;
         ballBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.scball);
         ballBitmap = Bitmap.createScaledBitmap(ballBitmap, ballSize, ballSize, false);
         brickBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.brick);
-
-
-
         ballPosition = new PointF(screenWidth / 2.0f, screenHeight * 3.0f / 4.0f);
         ballVelocity = new PointF(0, 0);
 
+        //벽돌
         bricks = new ArrayList<>();
         int brickWidth = screenWidth / 10;
         int brickHeight = screenHeight / 20;
         for (int i = 0; i < 10; i++) {
             RectF brick = new RectF(i * brickWidth, 0, (i + 1) * brickWidth, brickHeight);
             bricks.add(brick);
+
+            //드래그화살표
+            arrowPaint = new Paint();
+            arrowPaint.setColor(Color.RED);
+            arrowPaint.setColor(Color.RED);
+            arrowPaint.setStrokeWidth(5);
+
         }
     }
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (!ballIsMoving) {
-                    initialTouchX = event.getX();
-                    initialTouchY = event.getY();
-                }
+                initialTouch.set(event.getX(), event.getY());
+                currentTouch.set(event.getX(), event.getY());
+                drawArrow = true;
                 break;
+
+            case MotionEvent.ACTION_MOVE:
+                currentTouch.set(event.getX(), event.getY());
+                break;
+
             case MotionEvent.ACTION_UP:
-                if (!ballIsMoving) {
-                    float dx = event.getX() - initialTouchX;
-                    float dy = event.getY() - initialTouchY;
-                    float magnitude = (float) Math.sqrt(dx * dx + dy * dy);
-                    ballVelocity.set(-dx / magnitude * 20, -dy / magnitude * 20);
-                    ballIsMoving = true;
-                }
+                float deltaX = event.getX() - initialTouch.x;
+                float deltaY = event.getY() - initialTouch.y;
+                ballVelocity.set(-deltaX / 10, -deltaY / 10);
+                ballIsMoving = true;
+                drawArrow = false;
                 break;
         }
+        invalidate();
         return true;
     }
 
@@ -91,6 +109,14 @@ public class GameView extends View {
 
         for (RectF brick : bricks) {
             canvas.drawBitmap(brickBitmap, null, brick, paint);
+        }
+
+
+        //화살표
+        if (drawArrow) {
+            canvas.drawLine(initialTouch.x, initialTouch.y, currentTouch.x, currentTouch.y, arrowPaint);
+            canvas.drawCircle(initialTouch.x, initialTouch.y, 10, arrowPaint);
+            canvas.drawCircle(currentTouch.x, currentTouch.y, 10, arrowPaint);
         }
 
         if (ballIsMoving) {
@@ -120,6 +146,11 @@ public class GameView extends View {
             if (ballPosition.y >= screenHeight) {
                 ballIsMoving = false;
                 ballPosition.set(screenWidth / 2.0f, screenHeight * 3.0f / 4.0f);
+
+
+
+
+
             }
         }
         invalidate();
