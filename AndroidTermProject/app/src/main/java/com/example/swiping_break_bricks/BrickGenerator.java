794fc820d4a8bd286c8828d120e0e59d7eac349a
An Dae Hun <dkseogns88@naver.com>
2023-06-13 03:59:27 +0900

    벽돌이 스테이지에따라 생성및 체력증가

--- AndroidTermProject/app/src/main/java/com/example/swiping_break_bricks/BrickGenerator.java
+++ AndroidTermProject/app/src/main/java/com/example/swiping_break_bricks/BrickGenerator.java
@@ -8,45 +8,46 @@ import java.util.Random;
 public class BrickGenerator {
     private ArrayList<Brick> bricks;
     private int screenWidth, screenHeight;
-    private int brickWidth, brickHeight;
-    private Random random;
+    private int brickHeight;
+    private int brickWidth;
+    private int currentRowHealth = 1; // 첫 번째 줄의 벽돌 체력
+
 
     public BrickGenerator(int screenWidth, int screenHeight) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
-        this.brickWidth = screenWidth / 5;
-        this.brickHeight = screenHeight / 10;
+        this.brickHeight = screenHeight / 15;
+        this.brickWidth = screenWidth / 5; // 벽돌의 너비는 항상 화면 너비의 1/5
         this.bricks = new ArrayList<>();
-        this.random = new Random();
     }
 
     public ArrayList<Brick> getBricks() {
         return bricks;
     }
 
-    // 모든 벽돌을 한 줄 내리는 메서드
-    public void moveBricksDown(float distance) {
-        for (Brick brick : bricks) {
-            brick.rect.offset(0, distance);
-        }
-    }
+
 
 
     // 새로운 벽돌 줄을 생성하는 메서드
     public void generateNewRow() {
-        for (int i = 0; i < 5; i++) {
-            int health = random.nextInt(3) + 1; // 1~3의 랜덤한 체력
-            RectF rect = new RectF(i * brickWidth, 0, (i + 1) * brickWidth, brickHeight);
-            bricks.add(0, new Brick(rect, health)); // 새로운 벽돌을 리스트의 맨 앞에 추가
+        Random random = new Random();
+        int brickCount = random.nextInt(5) + 1; // 1개에서 5개 사이의 랜덤한 벽돌 개수
+        float top = 0;
+        if (!bricks.isEmpty()) {
+            top = bricks.get(0).rect.top - brickHeight; // 가장 위에 있는 벽돌의 위에 새로운 벽돌을 생성
         }
+        for (int i = 0; i < brickCount; i++) {
+            float left = i * brickWidth;
+            RectF rect = new RectF(left, top, left + brickWidth, top + brickHeight);
+            bricks.add(0, new Brick(rect, currentRowHealth)); // 리스트의 가장 앞에 벽돌 추가
+        }
+        currentRowHealth++; // 다음 줄의 벽돌 체력 증가
     }
 
     // 기존의 벽돌을 한줄내림
     public void moveBricksDown(int rows) {
         for (Brick brick : bricks) {
-            brick.rect.top += brickHeight * rows;
-            brick.rect.bottom += brickHeight * rows;
+            brick.rect.offset(0, rows * brickHeight);
         }
-
     }
 }
